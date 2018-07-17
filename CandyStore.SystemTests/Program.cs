9157ee7e6e241d19c3be2473e6046771aa432620
Mario Yordanov <mario.ivailov@abv.bg>
2018-07-17 22:18:37 +0300

    added SimpleInjector in SystemTests console app

--- CandyStore.SystemTests/Program.cs
+++ CandyStore.SystemTests/Program.cs
@@ -1,6 +1,8 @@
 ﻿using CandyStore.Contracts.Infrastructure;
 using CandyStore.DataModel.Models;
 using CandyStore.Infrastructure.Repositories;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
 using System;
 using System.Linq;
 
@@ -9,6 +11,13 @@ namespace CandyStore.SystemTests
     class Program
     {
         static void Main(string[] args)
+        {
+            //TestRepo();
+
+            TestScope();
+        }
+
+        private static void TestRepo()
         {
             ICandyStoreRepository candyStoreRepository = new CandyStoreRepository(new Infrastructure.CandyStoreDbContext());
 
@@ -24,7 +33,33 @@ namespace CandyStore.SystemTests
 
                 Console.WriteLine(string.Join(", ", customers.Select(x => $"{x.FirstName} {x.LastName}")));
             }
+        }
 
+        private static void TestScope()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new ThreadScopedLifestyle();
+
+            container.Register<Test>(Lifestyle.Scoped);
+
+            using (var scope = ThreadScopedLifestyle.BeginScope(container))
+            {
+                scope.Container.GetInstance<Test>().Number = 5;
+
+                var test = scope.Container.GetInstance<Test>();
+                Console.WriteLine(test.Number);
+            }
+
+            using (var scope = ThreadScopedLifestyle.BeginScope(container))
+            {
+                var test = scope.Container.GetInstance<Test>();
+                Console.WriteLine(test.Number);
+            }
+        }
+
+        public class Test
+        {
+            public int Number { get; set; }
         }
     }
 }
