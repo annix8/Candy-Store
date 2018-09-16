603de6673fe1a222d13a1d3334050a62d1b03cd6
Mario Yordanov <mario.ivailov@abv.bg>
2018-09-16 22:15:13 +0300

    added inner exception column

--- CandyStore.DataModel/Models/ExceptionLog.cs
+++ CandyStore.DataModel/Models/ExceptionLog.cs
@@ -8,5 +8,6 @@ namespace CandyStore.DataModel.Models
         public DateTime Date { get; set; }
         public string Message { get; set; }
         public string StackTrace { get; set; }
+        public string InnerException { get; set; }
     }
 }
